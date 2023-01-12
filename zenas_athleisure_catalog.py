ffa316e27ec4dd3bb7714fabe5880b24eecf1d6b
Minah Kim <minahkim4028@gmail.com>
2023-01-12 12:50:57 -0500

    display view catalog_for_website check

--- zenas_athleisure_catalog.py
+++ zenas_athleisure_catalog.py
@@ -5,4 +5,7 @@ import snowflake.connector
 #testing header on streamlit app
 st.header("Zena's Amazing Athleisure Catalog")
 conn = snowflake.connector.connect(**st.secrets["snowflake"])
-conn.close()
+cur = conn.cursor()
+cur.execute("SELECT * FROM CATALOG_FOR_WEBSITE")
+data_table = cur.fetchall()
+st.dataframe(data_table)
