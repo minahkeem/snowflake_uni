48ebc04bc75d8ddb5680e67e0a2b6e64e5a06273
Minah Kim <minahkim4028@gmail.com>
2023-01-12 12:31:36 -0500

    testing connection - github-streamlit-snowflake

--- zenas_athleisure_catalog.py
+++ zenas_athleisure_catalog.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+import snowflake.connector
 
+#testing header on streamlit app
+st.header("Zena's Amazing Athleisure Catalog")
+conn = snowflake.connector.connect(**st.secrets["snowflake"])
+conn.close()
